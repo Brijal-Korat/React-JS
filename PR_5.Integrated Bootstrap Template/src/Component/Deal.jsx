bec7e8741a7da288fa00e873fb58d0e9c8d94c34
Brijal-Korat <brijalkorat@gmail.com>
2024-10-13 02:54:55 +0530

    React-js

---
+++ PR_5.Integrated Bootstrap Template/src/Component/Deal.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Deal = () => {
+    return (
+        <section class="section-deal padding-b-100">
+            <div class="bg-banner-deal">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="cr-deal-rightside">
+                                <div class="cr-deal-content">
+                                    <span><code>35%</code> OFF</span>
+                                    <h4 class="cr-deal-title">Great deal on organic food.</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do maecenas accumsan
+                                        lacus
+                                        vel facilisis. </p>
+                                    <div id="timer" class="cr-counter">
+                                        <div class="cr-counter-inner">
+                                            <h4>
+                                                <span id="days">77</span>
+                                                Days
+                                            </h4>
+                                            <h4>
+                                                <span id="hours">13</span>
+                                                Hrs
+                                            </h4>
+                                            <h4>
+                                                <span id="minutes">13</span>
+                                                Min
+                                            </h4>
+                                            <h4>
+                                                <span id="seconds">34</span>
+                                                Sec
+                                            </h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Deal
---
+++ PR_5.Integrated Bootstrap Template/src/Component/Services.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <section className="section-services padding-b-100">
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-3">
+                        <div className="cr-services-border d-flex align-items-center ">
+                                    <div className="swiper-slide mx-2">
+                                        <div className="cr-services">
+                                            <div className="cr-services-image">
+                                                <i className="ri-red-packet-line" />
+                                            </div>
+                                            <div className="cr-services-contain">
+                                                <h4>Product Packing</h4>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="swiper-slide mx-2">
+                                        <div className="cr-services">
+                                            <div className="cr-services-image">
+                                                <i className="ri-customer-service-2-line" />
+                                            </div>
+                                            <div className="cr-services-contain">
+                                                <h4>24X7 Support</h4>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="swiper-slide mx-2">
+                                        <div className="cr-services">
+                                            <div className="cr-services-image">
+                                                <i className="ri-truck-line" />
+                                            </div>
+                                            <div className="cr-services-contain">
+                                                <h4>Delivery in 5 Days</h4>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="swiper-slide mx-2">
+                                        <div className="cr-services">
+                                            <div className="cr-services-image">
+                                                <i className="ri-money-dollar-box-line" />
+                                            </div>
+                                            <div className="cr-services-contain">
+                                                <h4>Payment Secure</h4>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default Services
