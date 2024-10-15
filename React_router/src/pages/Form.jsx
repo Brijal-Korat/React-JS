40ea2337f065ea4a45f6721aac8ede1f0006b8e0
Brijal-Korat <brijalkorat@gmail.com>
2024-10-16 04:15:51 +0530

    React-js

--- React_router/src/pages/Form.jsx
+++ React_router/src/pages/Form.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { json, Link, useNavigate } from 'react-router-dom'
 
 const Form = () => {
+
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const [allRecord, setAllRecord] = useState(localStorage.getItem("users") ? JSON.parse(localStorage.getItem("users")) : [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let obj = {
+      id : Math.floor(Math.random() * 10000),
+      name : name,
+      phone : phone
+    }
+
+    const newRecord = [...allRecord, obj];
+    localStorage.setItem("users", JSON.stringify(newRecord));
+    alert("Record added..!");
+    navigate('/');
+  }
+
   return (
     <div>
-        <h2>Add User</h2>
-        <input type="text" />
-        <Link to={"/"}>View</Link>
+        <h2>Add User React Router</h2>
+
+        <form onSubmit={handleSubmit}>
+          <table border={1}>
+              <tr>
+                <td>Name :- </td>
+                <td><input type="text" onChange={(e) => setName(e.target.value)} value={name} /></td>
+              </tr>
+              <tr>
+                <td>Phone :- </td>
+                <td><input type="text" onChange={(e) => setPhone(e.target.value)} value={phone} /></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td><input type="submit" /></td>
+              </tr>
+          </table>
+        </form>
+        <Link to={'/'}>View</Link>
     </div>
   )
 }
--- React_router/src/pages/Table.jsx
+++ React_router/src/pages/Table.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Table = () => {
+
+  const navigate = useNavigate();
+  const allUsers = JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : []
+
+  const [record, setRecord] = useState(allUsers);
+
+  const userDelete = (id) => {
+    let d = record.filter(val => val.id != id);
+    localStorage.setItem("users", JSON.stringify(d));
+    setRecord(d);
+    alert("Record deleted..!");
+  }
+
   return (
-    <div>
+    <div align="center">
       <h2>View User</h2>
-      <Link to={"/form"}>Add</Link>
+      
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            record.map((u, i) => {
+              const { id, name, phone} = u;
+              return(
+                <tr key={i}>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{phone}</td>
+                  <td>
+                    <button onClick={() => userDelete(id)}>Delete</button> || 
+                    <button onClick={() => navigate(`/edit`, {state : u})}>Edit</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+      <Link to={`/add`}>Add</Link>
     </div>
   )
 }
