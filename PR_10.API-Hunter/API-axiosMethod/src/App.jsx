f7256c577a638a6802bd27b63e659ad127346244
Brijal-Korat <brijalkorat@gmail.com>
2024-11-21 02:41:06 +0530

    React-js

--- PR_10.API-Hunter/API-axiosMethod/src/App.jsx
+++ PR_10.API-Hunter/API-axiosMethod/src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try{
+      let res = await axios.get(`https://dummyjson.com/products`);
+      setProducts(res.data.products)
+    }catch(err){
+      console.log(err);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  })
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h2 className="text-center my-4">API Hunter - Product List with Axios</h2>
+      <div className="table-responsive">
+        <table border={1} className="table table-striped table-hover">
+          <thead className="table-light">
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Title</th>
+              <th scope="col">Brand</th>
+              <th scope="col">Category</th>
+              <th scope="col">Tags</th>
+              <th scope="col">Price</th>
+              <th scope="col">Rating</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Return Policy</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              products.map((prdct, index) => (
+                <tr key={index}>
+                  <td>{prdct.id}</td>
+                  <td>{prdct.title}</td>
+                  <td>{
+                    prdct.brand == null ?
+                         <div>
+                          <span className="badge bg-danger">No Brand</span>
+                         </div>
+                      : prdct.brand
+                  }</td>
+                  <td>{prdct.category}</td>
+                  <td>{prdct.tags.join(`, `)}</td>
+                  <td>{prdct.price}</td>
+                  <td>{prdct.rating}</td>
+                  <td>{prdct.stock}</td>
+                  <td>{prdct.returnPolicy}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
