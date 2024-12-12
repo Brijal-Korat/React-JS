3de12d57bf9019b573daa2baa185965cf0f8a324
Brijal-Korat <brijalkorat@gmail.com>
2024-12-12 13:52:26 +0530

    React-js

--- PR_13.Google Authentication using Firebase/src/App.jsx
+++ PR_13.Google Authentication using Firebase/src/App.jsx
@@ -1,5 +1,7 @@
 import { signInWithPopup } from "firebase/auth"
 import { googleAuthProvider, auth } from "../firebase"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
 const Login = () => {
   const handleSubmit = async () => {
@@ -12,9 +14,19 @@ const Login = () => {
   }
   return (
     <div align="center">
-      <button onClick={handleSubmit}>
+      <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+        <button className="btn btn-light border shadow-sm d-flex align-items-center" onClick={handleSubmit}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+            alt="Google Logo"
+            style={{ width: "20px", height: "20px", marginRight: "10px" }}
+          />
+          Sign in with Google
+        </button>
+      </div>
+      {/* <button onClick={handleSubmit}>
         Sign In
-      </button>
+      </button> */}
     </div>
   )
 }
